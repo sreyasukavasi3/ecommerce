644c9b7fbde54d596341b34e312fc561b49294da
Sreya Sukhavasi <ssukhava@sundevils.asu.edu>
2023-06-20 12:48:11 -0700

    Added Routes

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+
+
+const data = {
+    products: [
+        {
+            name: "ralph lauren shirt",
+            slug: "ralph-lauren-shirt",
+            category: 'Shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 100,
+            brand: 'Ralph Lauren',
+            rating: 4.7,
+            numReviews: 481,
+            description: "Men's Long Sleeve Classic Oxford shirt",
+        },
+        {
+            name: "Gap hoodie",
+            slug: "gap-hoodie",
+            category: 'Hoodie',
+            image: '/images/p2.jpg',
+            price: 49.99,
+            countInStock: 120,
+            brand: 'GAP',
+            rating: 4.7,
+            numReviews: 493,
+            description: "Women's Logo Hoodie",
+        },
+        {
+            name: "Adidas Pants",
+            slug: "adidas-pants",
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 55,
+            countInStock: 300,
+            brand: 'Adidas',
+            rating: 4.3,
+            numReviews: 40,
+            description: "Men's Tiro Pants",
+        },
+        {
+            name: "Tory Burch Bag",
+            slug: "tory-burch-bag",
+            category: 'Bag',
+            image: '/images/p4.jpg',
+            price: 228,
+            countInStock: 50,
+            brand: 'Tory Bruch',
+            rating: 4.8,
+            numReviews: 11,
+            description: "Women's Cross Body Bag",
+        },
+    ]
+}
+
+export default data;
