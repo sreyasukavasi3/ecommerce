fdfcea8c15c6e93ecd3abada0659d395659a7768
Sreya Sukhavasi <ssukhava@sundevils.asu.edu>
2023-06-21 02:25:41 -0700

    frontend-updations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,35 @@ import data from "./data";
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Buyopia</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to = "/">
+                <Navbar.Brand> Buyopia</Navbar.Brand>
+              </LinkContainer>
+              
+            </Container>
+          </Navbar>
+          {/* <Link to="/">Buyopia</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen/>}/>
-            <Route path="/" element={<HomeScreen/>}/>
-            
-          </Routes>
-          
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen/>}/>
+              <Route path="/" element={<HomeScreen/>}/>
+            </Routes>
+          </Container>
         </main>
+
+        <footer className='text-center'>All rights reserved</footer>
       </div>
     </BrowserRouter> 
   );
